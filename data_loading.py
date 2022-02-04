85f1252c4edad5d56ea0d192e61734ccb9963259
Michal Shlapentokh-Rothman <michalsr25@gmail.com>
2022-02-03 19:25:38 -0500

    updated cifar10 test

--- data_loading.py
+++ data_loading.py
@@ -25,9 +25,10 @@ def unpickle(file):
 
 class CIFAR10(data.Dataset):
     folder = ''
-    def __init__(self,train=True,augmentations=None,test=False):
+    def __init__(self,train=True,augmentations=None,test=False,auto_aug=True):
         self.train = train
         self.test = test
+        self.auto_aug = auto_aug
         self.augmentations = augmentations
         self.augmentation_list = self.make_aug_dict()
         self.data = []
@@ -36,13 +37,13 @@ class CIFAR10(data.Dataset):
         #take 
         if not test:
             for i in range(1,6):
-                with open(f'/shared/rsaas/michal5/ohl_auto_aug/cifar-10-batches-py/data_batch_{i}', 'rb') as f:
+                with open(f'/home/michal/ohl_auto_aug/cifar-10-batches-py/data_batch_{i}', 'rb') as f:
                     entry = pickle.load(f, encoding='latin1')
                     self.data.append(entry['data'])
                     if 'labels' in entry:
                         self.labels.extend(entry['labels'])
         else:
-            with open(f'/shared/rsaas/michal5/ohl_auto_aug/cifar-10-batches-py/test_batch', 'rb') as f:
+            with open(f'/home/michal/ohl_auto_aug/cifar-10-batches-py/test_batch', 'rb') as f:
                 entry = pickle.load(f, encoding='latin1')
                 self.data.append(entry['data'])
                 if 'labels' in entry:
@@ -73,6 +74,18 @@ class CIFAR10(data.Dataset):
         transforms.Normalize(MEAN, STD),
     ])
         return t(img) 
+    def regular_transform(self,img):
+        #apply previous transforms 
+        #apply img augs
+        #apply cutout
+        MEAN, STD = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        t=  transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(MEAN, STD),
+    ])
+        return t(img) 
     def normal_transform(self,img):
         MEAN, STD = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
         t=  transforms.Compose([
@@ -90,11 +103,18 @@ class CIFAR10(data.Dataset):
         img = Image.fromarray(img)
 
   
-        if self.train:
-            augs = self.augmentations[index]
-            img_augs = self.get_custom_augs(augs)
-            img = self.apply_transform(img,img_augs)
+        if self.train==True:
+            if self.auto_aug:
+                #print('Loading auto aug')
+                augs = self.augmentations[index]
+                
+                img_augs = self.get_custom_augs(augs)
+                img = self.apply_transform(img,img_augs)
+            else:
+                #print('Loading normal train transforms')
+                img = self.regular_transform(img)
         else:
+            #print('Loading normal for val and test')
             img = self.normal_transform(img)
   
         return img, target
@@ -120,6 +140,119 @@ class CIFAR10(data.Dataset):
                 
 
 
+class CIFAR10Test(data.Dataset):
+    folder = ''
+    def __init__(self,train=True,augmentations=None,test=False,auto_aug=True):
+        self.train = train
+        self.test = test
+        self.auto_aug = auto_aug
+        self.augmentations = augmentations
+        self.augmentation_list = self.make_aug_dict()
+        self.data = []
+        self.labels = []
+        
+        #take 
+        if not test:
+            for i in range(1,6):
+                with open(f'/home/michal/ohl_auto_aug/cifar-10-batches-py/data_batch_{i}', 'rb') as f:
+                    entry = pickle.load(f, encoding='latin1')
+                    self.data.append(entry['data'])
+                    if 'labels' in entry:
+                        self.labels.extend(entry['labels'])
+        else:
+            with open(f'/home/michal/ohl_auto_aug/cifar-10-batches-py/test_batch', 'rb') as f:
+                entry = pickle.load(f, encoding='latin1')
+                self.data.append(entry['data'])
+                if 'labels' in entry:
+                    self.labels.extend(entry['labels'])
 
+        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
+        self.data = self.data.transpose((0, 2, 3, 1))  # convert to HWC
     
+    def get_samplers(self,valid_size=0.1):
+        num_train = len(self.data)
+        indices = list(range(num_train))
+        split = int(np.floor(valid_size * num_train))
+        train_idx, valid_idx = indices[split:], indices[:split]
+        train_sampler = SubsetRandomSampler(train_idx)
+        valid_sampler = SubsetRandomSampler(valid_idx)
+        return train_sampler,valid_sampler,train_idx
+    def apply_transform(self,img,img_augs):
+        #apply previous transforms 
+        #apply img augs
+        #apply cutout
+        MEAN, STD = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        t=  transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        img_augs[0],img_augs[1],
+        transforms.ToTensor(),
+        Cutout(n_holes=1, length=16),
+        transforms.Normalize(MEAN, STD),
+    ])
+        return t(img) 
+    def regular_transform(self,img):
+        #apply previous transforms 
+        #apply img augs
+        #apply cutout
+        MEAN, STD = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        t=  transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(MEAN, STD),
+    ])
+        return t(img) 
+    def normal_transform(self,img):
+        MEAN, STD = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        t=  transforms.Compose([
+        transforms.ToTensor(),   
+        transforms.Normalize(MEAN, STD),
+    ])
+        return t(img) 
+    def __len__(self):
+        return len(self.data)
+    def __getitem__(self,index):
+        img, target = self.data[index], self.labels[index]
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img)
+
+  
+        if self.train==True:
+            if self.auto_aug:
+                #print('Loading auto aug')
+                augs = self.augmentations[index]
+                
+                img_augs = self.get_custom_augs(augs)
+                img = self.apply_transform(img,img_augs)
+            else:
+                #print('Loading normal train transforms')
+                img = self.regular_transform(img)
+        else:
+            #print('Loading normal for val and test')
+            img = self.normal_transform(img)
+  
+        return img, target
+    def get_custom_augs(self,augs):
+        aug_1,aug_2 = self.augmentation_list[augs]
+       
+        return aug_1,aug_2
+    def make_aug_dict(self):
+   
+        aug_dict={0:ShearX(0.1),1:ShearX(0.2),2:ShearX(0.3),3:ShearY(0.1),4:ShearY(0.2),5:ShearY(0.3),6:TranslateX(0.15),
+        7:TranslateX(0.3),8:TranslateX(0.45),9:TranslateY(0.15),10:TranslateY(0.3),11:TranslateY(0.45),12:Rotate(10),
+        13:Rotate(20),14:Rotate(30),15:Color(0.3),16:Color(0.6),17:Color(0.9),18:Posterize(4),19:Posterize(5),20:Posterize(8),21:Solarize(26),22:Solarize(102),23:Solarize(179),
+        24:Contrast(1.3),25:Contrast(1.6),26:Contrast(1.9),27:Sharpness(1.3),28:Sharpness(1.6),29:Sharpness(1.9),30:Brightness(1.3),31:Brightness(1.6),32:Brightness(1.9),
+        33:AutoContrast(),34:Equalize(),35:Invert()}
+        index_start = 0
+        final_dict = {}
+        for k_1 in aug_dict.keys():
+            for k_2 in aug_dict.keys():
+                final_dict[index_start] = (aug_dict[k_1],aug_dict[k_2])
+                index_start += 1 
+        return final_dict 
+
+                
 
--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from argparse import ArgumentParser
 from socketserver import ThreadingUDPServer
 import torch
 import torch.utils.data
+import os 
 import torch.nn as nn
-from data_loading import CIFAR10
+from data_loading import CIFAR10,CIFAR10Test
 import logging 
+from torch.utils.tensorboard import SummaryWriter
 import torch.optim as optim
 from os import listdir, remove
 from shutil import rmtree
 from os.path import exists, isdir, join
 from tqdm import tqdm 
 import numpy as np 
+import pdb 
 import sys
 def get_yes_no(msg):
   while True:
@@ -56,18 +59,22 @@ class Weights(nn.Module):
       self.sigmoid = nn.Sigmoid()
       self.batch_size =256
       self.log_prob = []
-  def sample(self):
+  def sample(self,num_samples):
     lessons = []
     sample_log_probs = torch.zeros(1)
-
+    #pdb.set_trace()
     p = self.sigmoid(self.weights)
+    p = p/p.sum(dim=-1,keepdim=True )
+
     dist = torch.distributions.categorical.Categorical(probs=p)
-    for i in range(self.batch_size):
+    print(dist.mean,'dist mean')
+    for i in range(num_samples):
       lesson_1 = dist.sample()
-      lesson_2 = dist.sample()
-      lessons.append((lesson_1.item(),lesson_2.item()))
+
+      lessons.append(lesson_1.item())
+  
       sample_log_probs = torch.add(sample_log_probs,dist.log_prob(lesson_1))
-      sample_log_probs = torch.add(sample_log_probs,dist.log_prob(lesson_2))
+    print(sample_log_probs,'sample log probs') 
     self.log_prob.append(sample_log_probs)
     #print(self.log_prob,'after sampl')
     #print(trajec,len(self.log_prob))
@@ -120,6 +127,7 @@ def test(dataset,best_model_path,logger):
   correct = 0
   total = 0
   dataset.train=False 
+  dataset.test = True 
   model,optim,sched = initialize_model(best_model_path,300)
   test_loader = torch.utils.data.DataLoader(
         dataset, batch_size=256, shuffle=True,
@@ -138,7 +146,7 @@ def test(dataset,best_model_path,logger):
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
-  logging.info(f'Acc: {correct/total}')
+  logger.info(f'Acc: {correct/total}')
   print(f'Acc:{correct/total}')
   return correct/total 
 def val_trajec(dataset,val_sampler,model):
@@ -163,8 +171,8 @@ def val_trajec(dataset,val_sampler,model):
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
   return correct/total 
-def train_trajec(train_dataset,train_sampler,model,num_train,logger,optimizer,scheduler):
-    print(scheduler.get_last_lr())
+def train_trajec(train_dataset,train_sampler,model,num_train,logger,optimizer,scheduler,trajec_num,epoch_num,writer):
+
     train_dataset.train=True 
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=256, sampler=train_sampler,
@@ -195,44 +203,49 @@ def train_trajec(train_dataset,train_sampler,model,num_train,logger,optimizer,sc
         running_loss += loss.item()
         if i%100 == 0 and i>0:
           logger.info(f'Loss is {running_loss/i}')
+          writer.add_scalar(f'loss_trajec_{trajec_num}_epoch_{epoch_num}',running_loss/i,i)
 
         # print statistics
         
         # if i % 2000 == 1999:    # print every 2000 mini-batches
         #     print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
         #     running_loss = 0.0
+    writer.add_scalar(f'loss_trajec_{trajec_num}_epoch_{epoch_num}',running_loss/len(pbar),len(pbar))
+
     return model,optimizer,scheduler
 def initialize_model(best_model_path,epoch_num):
   
   model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
   model.to("cuda:0")
   optimizer = optim.SGD(model.parameters(), lr=0.2, momentum=0.9,weight_decay=.0005)
-  sched = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, int(300), eta_min=0, last_epoch=- 1, verbose=False)
-
+  sched = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, int(300))
+  
   if epoch_num>0:
-    state_dict = torch.load('/shared/rsaas/michal5/ohl_auto_aug/outputs/best_model.pt',map_location="cuda:0") 
+    state_dict = torch.load('/home/michal/ohl_auto_aug/outputs/best_model.pt',map_location="cuda:0") 
     model.load_state_dict(state_dict['model'])
     optimizer.load_state_dict(state_dict['optimizer'])
+   
     sched.load_state_dict(state_dict['scheduler'])
+    print(sched.get_last_lr(),'last lr')
   return model,optimizer,sched 
 def save_best_model(model,optimizer,scheduler,epoch,end_of_epoch=False):
   
   if end_of_epoch:
-    state_dict = torch.load('/shared/rsaas/michal5/ohl_auto_aug/outputs/temp_model.pt',map_location="cuda:0")
+    state_dict = torch.load('/home/michal/ohl_auto_aug/outputs/temp_model.pt',map_location="cuda:0")
     model.load_state_dict(state_dict['model'])
     optimizer.load_state_dict(state_dict['optimizer'])
     scheduler.load_state_dict(state_dict['scheduler'])
     scheduler.step()
     state_dict = {'model':model.state_dict(),'optimizer':optimizer.state_dict(),'scheduler':scheduler.state_dict(),'epoch':epoch}
-    torch.save(state_dict,'/shared/rsaas/michal5/ohl_auto_aug/outputs/best_model.pt')
+    torch.save(state_dict,'/home/michal/ohl_auto_aug/outputs/best_model.pt')
   else:
     state_dict = {'model':model.state_dict(),'optimizer':optimizer.state_dict(),'scheduler':scheduler.state_dict(),'epoch':epoch}
-    torch.save(state_dict,'/shared/rsaas/michal5/ohl_auto_aug/outputs/temp_model.pt')
+    torch.save(state_dict,'/home/michal/ohl_auto_aug/outputs/temp_model.pt')
     
 
-def train_epoch(weights,best_model_path,highest_reward,epoch_num,logger,inner_dict=None,resume=False):
+def train_epoch(weights,best_model_path,highest_reward,epoch_num,logger,cifar_10,writer,inner_dict=None,resume=False):
     best_model = None 
-    cifar_10 =CIFAR10(train=True)
+    
     if resume == False:
 
       rewards = []
@@ -241,6 +254,8 @@ def train_epoch(weights,best_model_path,highest_reward,epoch_num,logger,inner_di
       new_best_model_path = None
       highest_reward = -1
       start_trajec = 0
+      weights_step = 0
+      train_step = 0 
     else:
 
       rewards = inner_dict['reward_list']
@@ -251,6 +266,7 @@ def train_epoch(weights,best_model_path,highest_reward,epoch_num,logger,inner_di
       new_best_model_path = inner_dict['new_best_model_path']
       highest_reward = inner_dict['highest_reward']
       start_trajec = inner_dict['trajec']
+    
 
 
     
@@ -270,40 +286,41 @@ def train_epoch(weights,best_model_path,highest_reward,epoch_num,logger,inner_di
         
 
         num_samples = 45000/256
-        for i in range(int(num_samples)+1):
-          batch_augs = weights.sample() 
-          for b in batch_augs:
-            aug_list.append(b)
+        sampled_augs = weights.sample(45000)
+        for a in sampled_augs:
+          aug_list.append(a)
+       
         for i,v in enumerate(all_augmentations.keys()):
           all_augmentations[v] = aug_list[i]
 
         # print(len(all_augmentations),num_train)
         # assert len(all_augmentations) >= num_train
         cifar_10.augmentations = all_augmentations
-        trained_model,optimizer,scheduler = train_trajec(cifar_10,train_sampler,model,num_train,logger,optimizer,scheduler)
+        trained_model,optimizer,scheduler = train_trajec(cifar_10,train_sampler,model,num_train,logger,optimizer,scheduler,trajec,epoch_num,writer)
         reward = val_trajec(cifar_10,val_sampler,trained_model)
         logger.info(f'Reward for trajec {trajec} is {reward}')
         rewards.append(reward)
+        writer.add_scalar(f'rewards_epoch_{epoch_num}',reward,trajec)
         if reward > highest_reward:
           save_best_model(model,optimizer,scheduler,epoch_num)
           #scheduler.step()
           # state_dict = {'model':model.state_dict(),'optim':optimizer.state_dict(),'sched':scheduler.state_dict(),'epoch':epoch_num,'trajec':trajec}
-          # torch.save(state_dict,f'/shared/rsaas/michal5/ohl_auto_aug/outputs/temp_model.pt')
-          #new_best_model_path = f'/shared/rsaas/michal5/ohl_auto_aug/outputs/best_model.pt'
+          # torch.save(state_dict,f'/home/michal/ohl_auto_aug/outputs/temp_model.pt')
+          #new_best_model_path = f'/home/michal/ohl_auto_aug/outputs/best_model.pt'
           
           highest_reward =reward 
-        save_inner(trajec,epoch_num,all_augmentations,train_sampler,val_sampler,best_model_path,new_best_model_path,highest_reward,rewards,optimizer,scheduler)
-    
+        save_inner(trajec,epoch_num,all_augmentations,train_sampler,val_sampler,best_model_path,new_best_model_path,highest_reward,rewards,optimizer,scheduler,cifar_10)
+    writer.add_scalar(f'highest_reward',highest_reward,epoch_num)
     best_model_path = new_best_model_path
     save_best_model(model,optimizer,scheduler,epoch_num,end_of_epoch=True)
     return rewards,best_model_path,highest_reward,cifar_10
-def save_inner(trajec,epoch_num,all_augmentations,train_sampler,val_sampler,best_model_path,new_best_model_path,highest_reward,rewards,optim,sched):
+def save_inner(trajec,epoch_num,all_augmentations,train_sampler,val_sampler,best_model_path,new_best_model_path,highest_reward,rewards,optim,sched,cifar_10):
     save_dict = {'trajec':trajec,'epoch':epoch_num,'augmentations':all_augmentations,'train_sampler':train_sampler,'val_sampler':val_sampler,'best_model_path':best_model_path,
-    'new_best_model_path':new_best_model_path,'highest_reward':highest_reward,'reward_list':rewards,'optim':optim.state_dict(),'sched':sched.state_dict()}
-    torch.save(save_dict,'/shared/rsaas/michal5/ohl_auto_aug/outputs/training_loop.pt')
+    'new_best_model_path':new_best_model_path,'highest_reward':highest_reward,'reward_list':rewards,'optim':optim.state_dict(),'sched':sched.state_dict(),'augmentation_list':cifar_10.augmentation_list}
+    torch.save(save_dict,'/home/michal/ohl_auto_aug/outputs/training_loop.pt')
 def save_outer(weights):
     save_dict = {'weights':weights.state_dict(),'weight_optim':weights.optimizer.state_dict(),'weight_log_prob':weights.log_prob}
-    torch.save(save_dict,'/shared/rsaas/michal5/ohl_auto_aug/outputs/outer_loop.pt')
+    torch.save(save_dict,'/home/michal/ohl_auto_aug/outputs/outer_loop.pt')
 def compute_normalized_validation(rewards):
     new_rewards = []
     mean = np.mean(rewards)
@@ -317,35 +334,50 @@ def compute_normalized_validation(rewards):
 def main():
     parser = ArgumentParser()
     parser.add_argument("--resume",type=bool,default=None )
-    logger = setup_logger('logger','/shared/rsaas/michal5/ohl_auto_aug/outputs/log_file.log')
+    parser.add_argument("--auto_aug",type=bool,default=None)
+    logger = setup_logger('logger','/home/michal/ohl_auto_aug/outputs/log_file.log')
     args = parser.parse_args()
+    if args.auto_aug != None:
+      auto_aug = True 
+    else:
+      auto_aug = False
     highest_reward = -1 
+    cifar_10 = CIFAR10(train=True,auto_aug=auto_aug)
+    logger.info(f'Auto augmentation is {auto_aug}')
     best_model_path = None 
     start_epoch = 0
     if args.resume == None:
       
-      clear_if_nonempty('/shared/rsaas/michal5/ohl_auto_aug/outputs/')
+      clear_if_nonempty('/home/michal/ohl_auto_aug/outputs/')
     else:
-      inner_dict = torch.load('/shared/rsaas/michal5/ohl_auto_aug/outputs/training_loop.pt')
-      outer_dict = torch.load('/shared/rsaas/michal5/ohl_auto_aug/outputs/outer_loop.pt')
+      inner_dict = torch.load('/home/michal/ohl_auto_aug/outputs/training_loop.pt')
+      outer_dict = torch.load('/home/michal/ohl_auto_aug/outputs/outer_loop.pt')
       start_epoch = inner_dict['epoch']
   
     
-
-       
-    w = Weights(36,256)
+    if not os.path.exists('/home/michal/ohl_auto_aug/outputs/tensorboard'):
+      os.makedirs('/home/michal/ohl_auto_aug/outputs/tensorboard')
+    summary_writer = SummaryWriter('/home/michal/ohl_auto_aug/outputs/tensorboard')
+    w = Weights(36*36,256)
     if args.resume != None:
 
       w.load_state_dict(outer_dict['weights'])
+
     for i in range(start_epoch,300):
-      if args.resume != None:
-        rewards,best_model_path,highest_reward,dataset = train_epoch(w,best_model_path,highest_reward,i,logger,inner_dict=inner_dict,resume=True)
+      #summary_writer.add_scalar('average_weight',w.weights.mean(),i)
+      if args.resume != None and i<=start_epoch:
+        cifar_10.augmentation_list = inner_dict['augmentation_list']
+        rewards,best_model_path,highest_reward,dataset = train_epoch(w,best_model_path,highest_reward,i,logger,cifar_10,summary_writer,inner_dict=inner_dict,resume=True)
       else:
-         rewards,best_model_path,highest_reward,dataset = train_epoch(w,best_model_path,highest_reward,i,logger)
+        rewards,best_model_path,highest_reward,dataset = train_epoch(w,best_model_path,highest_reward,i,logger,cifar_10,summary_writer)
       normalized_rewards = compute_normalized_validation(rewards)
+      summary_writer.add_scalar('average weight',torch.mean(w.weights),i)
+      avg = torch.mean(w.weights).item()
+      logger.info(f'Average weight is {avg}')
       w.update(normalized_rewards)
       save_outer(w,)
-    test(dataset,best_model_path,logger)
+    cifar_10_test = CIFAR10Test(train=False,test=True)
+    test(cifar_10_test,best_model_path,logger)
     
       
     
